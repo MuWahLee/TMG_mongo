0a63f87c2ae6f4daf5bf6d1ea3c52e31c64f8b1d
Your NameMuWahLee <mlee2569@hotmail.com>
2014-03-12 13:59:22 +0800

    make additions to  seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+User.create([
+  {email: "mlee2569@hotmail.com"},
+  {email: "lee-mw@hotmail.com"},
+  {email: "mlee2569@gmail.com"},
+  ])
