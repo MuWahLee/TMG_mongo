502c1a43b08a16ec1496deed3b13a97bada332d8
Your NameMuWahLee <mlee2569@hotmail.com>
2014-03-12 16:05:16 +0800

    add encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,15 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
+  before_save :encrypt_password
+
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
+
+  def encrypt_password
+  end
 
 end
