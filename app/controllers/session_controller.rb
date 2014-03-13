c3bb23a85162248b49bcd138bd2d3ee3353a5d9a
Your NameMuWahLee <mlee2569@hotmail.com>
2014-03-13 11:05:44 +0800

    add session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,13 @@
+class SessionController < ApplicationController
+
+def new
+  render text: "Display the log in form"
+end
+
+def create
+  render text: "log the user in"
+end
+
+def destroy
+  render text: "log the user out"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 TallyMyGear::Application.routes.draw do
 
-  root 'site#index'
+  root    'site#index'
 
   get     'login'   => 'session#new'
   post    'login'   => 'session#create'
   delete  'logout'  => 'session#destroy'
   get     'logout'  => 'session#destroy' #TODO: remove before deployment
 
-  get 'privacy' => 'site#privacy'
-  get 'terms' => 'site#terms'
+  get     'privacy' => 'site#privacy'
+  get     'terms' => 'site#terms'
 
 end
