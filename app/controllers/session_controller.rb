c5afa411c30dc2a78390b8c2b219bf40fbed52aa
Your NameMuWahLee <mlee2569@hotmail.com>
2014-03-13 11:10:47 +0800

    correct session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,13 +1,15 @@
 class SessionController < ApplicationController
 
-def new
-  render text: "Display the log in form"
-end
+  def new
+    render text: "Display the log in form"
+  end
 
-def create
-  render text: "log the user in"
-end
+  def create
+    render text: "log the user in"
+  end
 
-def destroy
-  render text: "log the user out"
+  def destroy
+    render text: "log the user out"
+  end
+
 end
